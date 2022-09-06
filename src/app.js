cb6dd75d1280862152e13198b87281984553481c
aaron-meche <aaronmeche18@icloud.com>
2022-09-06 15:17:48 -0500

    Connected to Database

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// FIREBASE DATA
+
 import { 
     initializeApp 
 } from "firebase/app";
@@ -20,17 +22,26 @@ const firebaseConfig = {
   appId: "1:664695284118:web:e0a1456728c059a1f6bdb7"
 };
 
-
+// CONSTANTS AND EDITABLE VARIABLES
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase();
 
 var activePath;
+var calendarData;
+
+
+
 
 
 
 // Reference Functions
+
+
+
+
+
 
 
 
@@ -46,11 +57,48 @@ function writeData(path, value) {
     set(ref(database, path), value);
 }
 
-// onValue(ref(database, path), (snapshot) => {
 
-// }, {
-//     onlyOnce: true
-// });
+
+
+
+
+
+
+// Onload, get calendar data
+onValue(ref(database, 'calendars'), (snapshot) => {
+    calendarData = snapshot.val();
+
+    // If viewing a calendar
+    if (dom('calendar-space')) {
+        if (localStorage['personal calendar']) {
+            sessionStorage['active calendar'] = localStorage['personal calendar'];
+        } else {
+
+        }
+
+
+        // Show current year's calendar
+        buildCalendar(calendarYear, 0, 'US Holidays');
+        dom('month_' + months[date.getMonth()]).scrollIntoView();
+    
+
+        // Click listners for view year change
+        for (let i = 0; i < 2; i++) {
+            dom_c('modify-display-year')[i].addEventListener('click', function() {
+                calendarYear = calendarYear + Number(dom_c('modify-display-year')[i].getAttribute('title'));
+                buildCalendar(calendarYear, 0);
+            })
+        }
+    }
+
+
+}, {
+    onlyOnce: true
+});
+
+
+
+
 
 // Library
 const date = new Date();
@@ -59,27 +107,43 @@ let month_lengths = [31,28,31,30,31,30,31,31,30,31,30,31];
 let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 let calendarYear = date.getFullYear();
 
-// On Load, RUN...
-if (dom('calendar-space')) {
-    buildCalendar(calendarYear, 0);
-    dom('month_' + months[date.getMonth()]).scrollIntoView();
-    // dom('date_' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear()).style.fontWeight = '500';
 
-    for (let i = 0; i < 2; i++) {
-        dom_c('modify-display-year')[i].addEventListener('click', function() {
-            calendarYear = calendarYear + Number(dom_c('modify-display-year')[i].getAttribute('title'));
-            buildCalendar(calendarYear, 0);
-        })
+
+
+
+
+// Add events to calendar
+function insertEvents(calendar) {
+    let objectRef = calendarData[calendar]['events'];
+    let eventCount = Object.keys(objectRef).length;
+    // if (objectRef) {
+    //     eventCount = Object.keys(objectRef).length;
+    //     console.log(m + '-' + d + '-' + y, eventCount)
+    // }
+
+    for (let i = 0; i < eventCount; i++) {
+        let date = 'date_' + Object.keys(objectRef)[i];
+        let values = Object.values(objectRef)[i];
+        let event_display = '';
+        for (let i = 0; i < Object.keys(values).length; i++) {
+            event_display = event_display + '●';
+        }
+        dom(date).children[0].children[1].innerHTML = event_display;
+        // let eventDisplay = '';
+        // eventDisplay = eventDisplay + '●';
     }
-
-    // dom_c('current-year-display')[0].addEventListener('click', function() {
-    //     calendarYear = prompt('What year do you want to transport to?');
-    //     buildCalendar(calendarYear, 0);
-    // })
+    // console.log('Events Inserted')
 }
 
+
+
+
+
+
+
+
 // Calendar Builder
-function buildCalendar(year_begin, extra_years) {
+function buildCalendar(year_begin, extra_years, calendar) {
     dom('calendar-space').innerHTML = '';
     dom_c('current-year-display')[0].innerHTML = calendarYear;
     for (let y = year_begin; y < (year_begin + extra_years + 1); y++) {
@@ -92,12 +156,19 @@ function buildCalendar(year_begin, extra_years) {
                 generated_month_data = generated_month_data + `<div class='calendar-object day empty'></div>`;
             }
 
-            function insertDayWithTag(m, d, y, wd) {
-                let randomNum = Math.floor(Math.random() * 2);
+            function insertDayWithTag(m, d, y) {
+                // let eventCount = 0;
+                // let objectRef = calendarData[sessionStorage['active calendar']]['events'][m + '-' + d + '-' + y];
+                // if (objectRef) {
+                    // eventCount = Object.keys(objectRef).length;
+                    // console.log(m + '-' + d + '-' + y, eventCount)
+                // }
+
                 let eventDisplay = '';
-                for (let i = 0; i < randomNum; i++) {
-                    eventDisplay = eventDisplay + '●';
-                }
+                // for (let i = 0; i < eventCount; i++) {
+                    // eventDisplay = eventDisplay + '●';
+                // }
+
                 generated_month_data = generated_month_data + `
 
                 <div class='calendar-object day' id='date_` + (Number(m) + 1) + `-` + d  + `-` + y + `'>
@@ -161,4 +232,5 @@ function buildCalendar(year_begin, extra_years) {
         }
     }
     dom('date_' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear()).classList.add('current-day');
+    insertEvents(calendar);
 }
